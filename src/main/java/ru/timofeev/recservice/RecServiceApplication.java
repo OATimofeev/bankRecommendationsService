5588f9f2571f1fcb00bcbd3a2b73ae3688830fcd
Oleg Timofeev <oleg.timofeev@rtmis.ru>
2026-06-04 01:22:45 +0300

    Base config for db

---
+++ src/main/java/ru/timofeev/recservice/RecServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.timofeev.recservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/timofeev/recservice/RecServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timofeev.recservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
